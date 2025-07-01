7494f9fceff95cf58c21ae3b673ef517c26513c1
Matt Faltyn <faltyn.matthew@gmail.com>
2025-06-30 22:14:25 -0700

    Ignore SCSS depreciation

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/soccer_analytics_product/',
+  base: "/soccer_analytics_product/",
   server: {
     open: true,
     port: 3000,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+        silenceDeprecations: ["mixed-decls"],
+      },
+    },
   },
 });
